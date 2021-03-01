ff052bfee537da65bbf2aa30384de12b20aa3f51
playscript-js <76456024+playscript-js@users.noreply.github.com>
2021-03-02 01:56:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 app=require("express")();
 _=require("lodash");
-yt=require("youtube-sr").default;
+yt=require("youtube-search");
 url=require("ytdl-core");
-fs=require("fs");
-cors=require("cors");
+cors=require("cors")
+request=require("request")
 
 app.use(cors())
-
+opt={key: "AIzaSyCg4WbYR_fegTYMmS5jqijjRN8xc-Ce1mo"}
 
 
 app.get("/search/:query",(req,res)=>{
-yt.search(_.replace(req.params.query,"+"," "),{limit:0})
-.then(ret=>{res.json(ret)})
-.catch(e=>{res.send(e)})
+yt(_.replace(req.params.query,"+"," "),opt,(err,ret)=>{
+if(err)res.status(200).json([]); else{
+ls=[];
+for(var i=0;i<ret.length;i++){
+if(ret[i].kind=="youtube#video"){
+ls.push(ret[i])}}
+res.json(ls)}})
 })
 
 app.get("/video/:query",(req,res)=>{
-
-url.getInfo(req.params.query)
-.then(e=>{res.send(e.formats[e.formats.length-1].url||"null")})
+url.getInfo("https://www.youtube.com/watch?v="+
+req.params.query).then(e=>{
+ var u =e.formats[e.formats.length-1].url
+console.log(e.formats[e.formats.length-1])
+ req.pipe(request(u)).pipe(res);
 })
+ })
 
 app.listen(process.env.PORT||3000)
 console.log("ready")
